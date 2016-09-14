18788c86c0271034da9467840f51135fe5dc1e39
Shruti Appiah <shruti.appiah@gmail.com>
2016-09-14 17:11:03 -0400

    edit confpy

--- docs/weifund/conf.py
+++ docs/weifund/conf.py
@@ -42,6 +42,14 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
